5bfdf83d8653c5490a528cb9ef7ebd7e38dedab7
teddyybeaar <teddy.085018010@unpak.ac.id>
2022-08-22 16:53:39 +0700

    update

---
+++ server/routes/Users.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json("success");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Users.findOne({ where: { username: username } });
+
+  if (!user) res.json({ error: "Users doesn't exist!" });
+  else {
+    bcrypt.compare(password, user.password).then((match) => {
+      if (!match)
+        res.json({ error: "Wrong username and password combination!" });
+      else {
+        const accessToken = sign(
+          { username: user.username, id: user.id },
+          "importantsecret"
+        );
+        res.json(accessToken);
+      }
+    });
+  }
+});
+
+module.exports = router;
